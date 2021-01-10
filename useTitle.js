1eb2c47408dcf9b3744699f1fe72365439b981b4
501501 <solj6574@gmail.com>
2021-01-10 18:55:43 +0900

    react hooks

---
+++ useTitle.js
@@ -0,0 +1,10 @@
+export const useTitle = (initialTitle) => {
+    const [title, setTitle] = useState(initialTitle);
+    const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerHTML = title;
+    };
+
+    useEffect(updateTitle, [title]);
+    return setTitle;
+    }
